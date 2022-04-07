765a70da94ad9ddc7af1f101dc0398b541fc0d6f
harshv72 <52853300+harshv72@users.noreply.github.com>
2022-04-07 15:26:00 -0700

    Implemented Navbar
    - Navbar consists of logo and a searchbar to search for a hotel franchise

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Main from "./components/Main";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+//App Component
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //Use Browser Router to route to different pages
+    <BrowserRouter basename="/">
+      <div>
+        {/* App Component Has a Child Component called Main*/}
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ frontend/src/components/Pages/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../shared/Navbar';
+
+export default function LandingPage(props) {
+
+        return(
+            <div>
+                <Navbar isPageFavorite={false} isCartPage={false}/>
+            </div>
+            
+        )
+
+}
+
