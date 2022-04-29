c67fb239008d92a9a69f30ac06f968354c9d3aab
harshv72 <52853300+harshv72@users.noreply.github.com>
2022-04-28 21:11:12 -0700

    Updated Landing Screen
    - Updated Landing Screen; commented old design; initialized banner component design

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -1,27 +1,32 @@
 import React from "react";
+import Banner from "../components/Banner";
 import { BrowserRouter, Route, Link } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css"; // You can also use <link> for styles
-// ..
-AOS.init({
-  duration: 2000,
-});
+// import AOS from "aos";
+// import "aos/dist/aos.css"; // You can also use <link> for styles
+// // ..
+// AOS.init({
+//   duration: 2000,
+// });
 
 function LandingScreen() {
   return (
-    <div className="row landing">
-      <div className="col-md-12 text-center">
-        <h2 data-aos="zoom-in" style={{ color: "white", fontSize: "80px", fontFamily:"inherit" }}>
-          HOTEL BOOKING
-        </h2>
-        <h1 data-aos="zoom-out" style={{ color: "white", fontFamily:"inherit" }}>
-          Click below to catch up with our website!
-        </h1>
-        <Link to="/home">
-          {/* <button className="btn btn-primary landingBtn">Get Started</button> */}
-          <button className="button button1">Get Started</button>
-        </Link>
-      </div>
+    // <div className="row landing">
+    //   <div className="col-md-12 text-center">
+    //     <h2 data-aos="zoom-in" style={{ color: "white", fontSize: "80px", fontFamily:"inherit" }}>
+    //       HOTEL BOOKING
+    //     </h2>
+    //     <h1 data-aos="zoom-out" style={{ color: "white", fontFamily:"inherit" }}>
+    //       Click below to catch up with our website!
+    //     </h1>
+    //     <Link to="/home">
+    //       {/* <button className="btn btn-primary landingBtn">Get Started</button> */}
+    //       <button className="button button1">Get Started</button>
+    //     </Link>
+    //   </div>
+    // </div>
+
+    <div className='home'>
+            <Banner />
     </div>
   );
 }
