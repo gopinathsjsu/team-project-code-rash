f02ae4033652623df4c5df69b7f317de7db5fb2d
harshv72 <52853300+harshv72@users.noreply.github.com>
2022-05-04 20:12:40 -0700

    Added Cards in Landing Page
    - Added Cards in landing page for different rooms

--- frontend/src/screens/LandingScreen.js
+++ frontend/src/screens/LandingScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Banner from "../components/Banner";
+import Card from "../components/Card";
+import "../styles/Home.css";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 // import AOS from "aos";
 // import "aos/dist/aos.css"; // You can also use <link> for styles
@@ -27,6 +29,26 @@ function LandingScreen() {
 
     <div className='home'>
             <Banner />
+            <div className='home__section'>
+            <Card
+                src="https://i.pinimg.com/originals/45/3f/1a/453f1a6859e13ae5608e24de26a796b5.jpg"
+                title="Single Bedrooms"
+                description="Well-designed single bedrooms for your office trips"
+                price="Starting from $30/night"
+            />
+            <Card
+                src="https://cdn.decoist.com/wp-content/uploads/2012/06/Double-master-bedroom-in-Aspen-home.jpg"
+                title="Double Bedrooms"
+                description="Enjoy the amazing sights of California with these stunning double bedrooms"
+                price="Starting from $50/night"
+            />
+            <Card
+                src="https://media.nomadicmatt.com/2018/apartment.jpg"
+                title="Duplex Bedrooms"
+                description="Luxurious Rooms with vast spaces just for your long weekends"
+                price=" Starting from $70/night"
+            />
+            </div>
     </div>
   );
 }
